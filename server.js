935b46dc9949c4c486d0b4e64337404ec8bda8d4
Michelle Johnson <michellechristinejohnson@gmail.com>
2020-04-29 17:55:27 -0700

    set up project environment
    install express, body-parser, & cors.
    create a local server.

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+var express = require('express');
 // Start up an instance of app
+var app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors'); 
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
+const port = 8000; 
+// Spin up the server
+const server = app.listen(port, listening); 
+// Callback to debug
+function listening() { 
+    console.log("server is listening"); 
+    console.log(`running on localhost: ${port}`); 
+}; 
+// Initialize all route with a callback function
 
-// Setup Server
+// Callback function to complete GET '/all'
+
+// Post Route
+  
