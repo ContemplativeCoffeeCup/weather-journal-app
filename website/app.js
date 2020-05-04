775109922c1f730be80af6f2f6e532b9ccf71d13
Michelle Johnson <michellechristinejohnson@gmail.com>
2020-05-04 03:38:38 -0700

    Added API credentials

--- website/app.js
+++ website/app.js
@@ -1,7 +1,9 @@
 /* Global Variables */
+const zipCode = "";
+const URL = `api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=${apiKey}`;
 
 // Personal API Key for OpenWeatherMap API
-
+const apiKey = "420696289f3240255704d1ea6c52eadd";
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
