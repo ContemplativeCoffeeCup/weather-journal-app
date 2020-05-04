3feafc64143997245d37468eb36a333662e670f4
Michelle Johnson <michellechristinejohnson@gmail.com>
2020-05-04 02:28:16 -0700

    add POST route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ projectData = {};
 
 // Require Express to run server and routes
 var express = require('express');
+
 // Start up an instance of app
 var app = express();
 
@@ -21,15 +22,17 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-const port = 8000; 
 // Spin up the server
+const port = 8000; 
 const server = app.listen(port, listening); 
+
 // Callback to debug
 function listening() { 
     console.log("server is listening"); 
     console.log(`running on localhost: ${port}`); 
 }; 
 
+/* Middleware*/
 // Initialize all route with a callback function
 function sendData (req, res) { 
   res.send(projectData); 
@@ -38,8 +41,15 @@ function sendData (req, res) {
 // Callback function to complete GET '/all'
 app.get('/all', sendData); 
 
-// Post Route
-
-
-// app.post('/flavor', addFlavor);  
+/* Post Route */
+// An empty array to hold data
+const data = [];
+// post() with URL path and callback function
+app.post( '/add', addData )
+// callback function, receiving temp, date, and response
+function addData (req, res) {
+  console.log(req.body)
+  data.push(req.body)
+  console.log(data); 
+}
 
