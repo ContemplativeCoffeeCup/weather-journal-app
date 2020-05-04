22b7fee6efdb0cef53a57d379d47d89fd669b352
Michelle Johnson <michellechristinejohnson@gmail.com>
2020-05-04 16:33:42 -0700

    Async GET with fetch
    Write an async function in app.js that uses fetch() to make a GET request to the OpenWeatherMap API.

--- website/app.js
+++ website/app.js
@@ -1,18 +1,25 @@
 /* Global Variables */
-const zipCode = "";
-const URL = `api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=${apiKey}`;
-
-// Personal API Key for OpenWeatherMap API
-const apiKey = "420696289f3240255704d1ea6c52eadd";
+const zipCode = "98502";
+const apiKey = "&appid=d9d904f2868225960afa8e681c2aeb72";
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+const units = "&units=imperial";
+const weatherURL = baseURL+zipCode+units+apiKey;
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-
 // Event listener to add function to existing HTML DOM element
 
 /* Function called by event listener */
 
 /* Function to GET Web API Data*/
+
+getWeatherData();
+
+async function getWeatherData() {
+    const response = await fetch(weatherURL);
+    const data = await response.json();
+    console.log(data);
+}
 
 /* Function to POST data */
 
